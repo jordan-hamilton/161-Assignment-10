e979b5f8852c4400c4c1932862fdfd3901d58a2c
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-03-10 21:45:49 -0600

    Method templates

--- Store.cpp
+++ Store.cpp
@@ -3,3 +3,33 @@
  ** Date: 03/08/2018
  ** Description: Class implementation file template.
  *********************************************************************/
+
+#include "Store.hpp"
+
+void Store::addProduct(Product* ptrProduct) {
+
+}
+
+void Store::addMember(Customer* ptrCustomer) {
+
+}
+
+Product* Store::getProductFromID(std::string idCodeIn) {
+
+}
+
+Customer* Store::getMemberFromID(std::string accountIDIn) {
+
+}
+
+std::vector<std::string> Store::productSearch(std::string str) {
+
+}
+
+std::string Store::addProductToMemberCart(std::string pID, std::string mID) {
+
+}
+
+double Store::checkOutMember(std::string mID) {
+
+}
