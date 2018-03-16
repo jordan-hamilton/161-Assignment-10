9e2c268dc3caac12a8a7b7ec204918a517b08dac
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-03-15 22:01:29 -0600

    Added implementation descriptions

--- Customer.cpp
+++ Customer.cpp
@@ -1,7 +1,11 @@
 /*********************************************************************
  ** Author: Jordan Hamilton
- ** Date: 03/08/2018
- ** Description: Class implementation file template.
+ ** Date: 03/15/2018
+ ** Description: This implements a class representing a customer. Data
+ ** members represent the customer's shopping cart, name, account ID,
+ ** and whether or not he or she is a premium member of a store.
+ ** Methods add products to the customer's cart by product code, and
+ ** remove all items from the cart.
  *********************************************************************/
 
  #include "Customer.hpp"
@@ -20,6 +24,10 @@
    return cart;
  }
 
+/*********************************************************************
+** Description: Adds a string representing a product code to the end
+** of the cart vector.
+*********************************************************************/
  void Customer::addProductToCart(std::string idCodeIn) {
    cart.push_back(idCodeIn);
  }
@@ -28,6 +36,10 @@
    return premiumMember;
  }
 
+/*********************************************************************
+** Description: Empties the cart vector using the vector's built-in
+** clear function.
+*********************************************************************/
  void Customer::emptyCart() {
    cart.clear();
  }
